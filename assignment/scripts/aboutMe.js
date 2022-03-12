961a2134ca825f0303b27dcf67da1bf9925352b5
Pete Hall <pete.mack.hall@gmail.com>
2022-03-12 17:52:24 -0600

    First pass at aboutMe file and added initial questions to index.

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,44 +1,65 @@
 // REQUIRED FEATURES:
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-
+const firstName = 'Pete';
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
-
+const lastName = 'Hall';
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
-
+const fullName = firstName + ' ' + lastName;
 // 4 - Console log the value of `fullName`
-
+console.log('full name =', fullName);
 // 5 - Create a variable called `luckyNumber` and assign it the value of your lucky number.
-
+const luckyNumber = 2;
+console.log('lucky number =', luckyNumber);
 // 6 - Console log this sentence, adding in the variables you created above: 
 // 'My name is (full name), and I think (lucky number) is a winner!'.
 // Refer back to the videos if you need help with this one.
-
+console.log('My name is', fullName, 'and I think', luckyNumber, 'is a winner!');
 // 7 - Create a variable named `adventurous` and set it to a boolean value (true or false)
-
+const adventurous = true;
+console.log('The value of adventurous =', adventurous);
 // 8 - Create a variable named `food`, and set its value to a string of your favorite food
-
+const food = 'corn dogs';
+console.log('The value of food =', food);
 // 9 - Create a variable called `pets` and set it to the value of the number of pets you have
-
+let pets = 0;
+console.log('The value of pets =', pets);
 // 10 - Create a variable called `friendsPets` and assign it the value of the number of pets your friend has
-
+const friendsPets = 1;
+console.log('The value of friendsPets =', friendsPets);
 // 11 - Add two pets to your `pets` variable
-
+pets += 2;
+console.log('The new value of pets =', pets);
 // 12 - Create a constant variable called `allowedPets` and set it to a number value of your choice
-
+const allowedPets = 3
+console.log('The value of allowedPets =', allowedPets);
 // 13 - Create a conditional: if adventurous is true, console log "Adventures are great!", 
 // if it's not true, console log "How about we stay home?"
-
-
+if (adventurous === true) {
+  console.log('Adventures are great!');
+}
+else {
+  console.log('How about we stay home?');
+}
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
-
+if (luckyNumber === 2 && adventurous === true) {
+  console.log('Roll the dice!')
+}
 // 15 - Write a conditional that console logs "I can have more pets!" 
 //if the value of `pets` is less than the value of `allowedPets`,
 // console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
 // and console logs "Oh no, I have too many pets!"
 // if the value of `pets` is greater than the value of `allowedPets`.
-
+if (pets < allowedPets) {
+  console.log('I can have more pets');
+}
+else if (pets === allowedPets) {
+  console.log('I have enough pets');
+}
+else {
+  console.log('Oh no, I have too many pets!');
+}
 // STRETCH GOALS:
 
 // 16 - Make a variable called `mostPets` and a conditional that
@@ -46,6 +67,20 @@
 // assigns the highest value to `mostPets`. There's several possibilities --
 // be sure to think through all the scenarios. 
 // console.log `mostPets` after the conditional has run.
+let mostPets = undefined;
+console.log('The value of most pets =', mostPets);
+if (pets > friendsPets) {
+  mostPets = pets;
+} 
+else if (pets < friendsPets) {
+  mostPets = friendsPets;
+}
+else {
+  mostPets = pets;
+}
+console.log('The value of pets =', pets);
+console.log('The value of friendsPets =', friendsPets);
+console.log('The new value of mostPets =', mostPets);
 
 // 17 - Write a *switch* statement that logs:
 //      "First is the worst" if your lucky number is 1
